bf8218f8688465e7812b1c5b2ddbc2d5e4254803
Joshua Levin <joshua.c.levin@gmail.com>
2018-07-07 12:38:52 +0100

    start setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as ngrok from "ngrok";
+import * as df from 'dialogflow-fulfillment';
+import express from 'express';
+
+async function init() {
+    // Start ngrok if not deployed
+    const url = await ngrok.connect(80);
+    console.log(url);
+    console.log("bot running");
+    const app = express();
+    app.listen(3000, () => console.log('Example app listening on port 3000!'));
+    // const WebhookClient = df.WebhookClient;
+    app.get('/', () => {
+        console.log('called');
+    });
+}
+
+init();
